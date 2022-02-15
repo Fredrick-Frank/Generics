5c2085f007524f4e5e7c47f566df4f7c08b98543
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-02-15 16:32:31 +0100

    Add files via upload

---
+++ Generics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class DataStore<T> //generic class DataStore
+    {
+        public T Data { get; set; }
+
+        private T[] _data = new T[10];
+        public void AddUp(int index, T item)
+        {
+            if(index >=0 && index < 10)
+                _data[index] = item;
+        }
+        public T GetData(int index)
+        {
+            if(index >=0 && index < 10)
+                return _data[index];
+            else
+                return default(T);
+        }
+
+    }
+
+    class KeyValuePair<TKey, TValue> //having multiple type parameter 
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataStore<string> store = new DataStore<string>();
+            store.Data = "Hi";
+
+            Console.WriteLine(store.Data);
+
+            DataStore<int> intStore = new DataStore<int>();
+            intStore.Data = 200;
+            Console.WriteLine(intStore.Data);
+
+            KeyValuePair<int, string> kvp1 = new KeyValuePair<int, string>();
+            kvp1.Key = 100;
+            kvp1.Value = "Hundred";
+            Console.WriteLine(kvp1.Key + " " + kvp1.Value);
+
+            KeyValuePair<string, string> kvp2 = new KeyValuePair<string, string>();
+            kvp2.Key = "IT";
+            kvp2.Value = "Information Technology";
+            Console.WriteLine(kvp2.Key + " " + kvp2.Value);
+
+            DataStore<string> cities = new DataStore<string>();
+            cities.AddUp(0, "Mumbai");
+            cities.AddUp(1, "Chicago");
+
+            Console.WriteLine(cities.GetData(100));
+
+            DataStore<int> empIds = new DataStore<int>();
+            empIds.AddUp(0, 50);
+            empIds.AddUp(1, 25);
+            empIds.AddUp(2, 89);
+
+            Console.WriteLine(empIds.GetData(0));
+        }
+
+    }
+    
+}
